426559fe77a2f4c4bf1e314b8798bdda82124dc7
danushka96 <danushkaherath96@gmail.com>
2018-07-02 23:46:34 +0530

    final view

--- chat.php
+++ chat.php
@@ -1,4 +1,21 @@
 #!/usr/bin/php
+
+                               _________
+                              / ======= \
+                             / __________\
+                            | ___________ |
+                            | | -       | |
+                            | |         | |
+                            | |_________| |________________________
+                            \=____________/   Hello World          )
+                            / """"""""""" \                       /
+                            / ::::::::::::: \                  =D-'
+                            (_________________)
+
+                       Welcome to Secret Chat Service
+                              (Chat like Pro)
+                         
+
 <?php
 
 require('vendor/autoload.php');
@@ -14,27 +31,70 @@ $pubnub=new Pubnub(
     false
 );
 
-//get username
-$connectionAs = function(){
-    fwrite(STDOUT, 'Connect as: ');
-    return trim(fgets(STDIN));
-};
-
-$username=$connectionAs();
-
 //get rooms name
 fwrite(STDIN, 'join room: ');
 $room=trim(fgets(STDIN));
+
+fwrite(STDOUT,"Please Wait While we are checking data from the Channel\n");
+
+$herenow=$pubnub->herenow($room, false, true);
+
+
+//get username
+function connectAs(){
+    global $herenow;
+
+    fwrite(STDOUT, "\nConnect as: ");
+
+    $username=trim(fgets(STDIN));
+
+    foreach($herenow['uuids'] as $user){
+        if($user['state']['username']===$username){
+            fwrite(STDOUT,"username taken\n");
+            $username=connectAs();
+        }
+    }
+
+    return $username;
+};
+
+$username=connectAs();
+
+$pubnub->setState($room, ['username'=>$username]);
+
+
+fwrite(STDOUT, "Connected to '{$room}' as '{$username}'\n");
 
 $pid = pcntl_fork();
 
 if($pid==-1){
     exit(1);
 }elseif($pid){
-    //
+    
+    fwrite(STDOUT, '>');
+
+    while(true){
+        
+        $message=trim(fgets(STDIN));
+        $pubnub->publish($room, [
+
+            "body" => $message,
+            "username" => $username,
+
+        ]);
+    }
+
+    pcntl_wait($status);
 }else{
-    $pubnub->subscribe($room, function ($payload){
-        var_dump($payload);
+    $pubnub->subscribe($room, function ($payload) use ($username){
+        $timestamp=date('d-m-y H:i:s');
+
+        if($username!=$payload['message']['username']){
+            fwrite(STDOUT,"\r");
+        }
+
+        fwrite(STDOUT, "[{$timestamp}] <{$payload['message']['username']}> {$payload['message']['body']}\n");
+        fwrite(STDOUT, "\r>");
         return true;
     });
 }
